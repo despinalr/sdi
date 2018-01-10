6a12e097de26688f62177bb961bdb7cbb47cf9b9
despinalr <davidesr7@gmail.com>
2018-01-10 18:59:59 +0000

    Fix template

--- client/js/app/routes.js
+++ client/js/app/routes.js
@@ -2,28 +2,22 @@ module.exports = function (app) {
     
     var fs = require('fs');
     
-    function graph() {
+    app.get('/', function (req, res) {
 	    fs.readFile(__dirname + '/../../html/investments.html', 'utf8', function(err, text) {
-	        console.log('DavidE Text: ' + text);
-	        return text;
-        });
-    }
-
-	app.get('/', function (req, res) {
-	    fs.readFile(__dirname + '/../../html/investments.html', 'utf8', function(err, text) {
-	        console.log('DavidE Text 1: ' + text);
 	        res.render('index', {
         	    title : 'SDI - Sport Development Intelligence',
-        		graph: 'mapContainer'
+        		graph: text
         	});
         });
 	});
 	
 	app.get('/resources', function (req, res) {
-		res.render('index', {
-			title : 'SDI - Sport Development Intelligence',
-			graph: 'bubbleContainer'
-		});
+	    fs.readFile(__dirname + '/../../html/resources.html', 'utf8', function(err, text) {
+	        res.render('index', {
+        	    title : 'SDI - Sport Development Intelligence',
+        		graph: text
+        	});
+        });
 	});
 	
 };
