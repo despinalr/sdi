a57695e3ff2d9a409524d626875a8680ddcaac46
despinalr <davidesr7@gmail.com>
2018-01-11 18:35:32 +0000

    Info

--- client/js/app/routes.js
+++ client/js/app/routes.js
@@ -20,4 +20,58 @@ module.exports = function (app) {
         });
 	});
 	
+	app.get('/who', function (req, res) {
+	    fs.readFile(__dirname + '/../../html/who.html', 'utf8', function(err, text) {
+	        res.render('index', {
+        	    title : 'SDI - Sport Development Intelligence',
+        		graph: text
+        	});
+        });
+	});
+	
+	app.get('/mision', function (req, res) {
+	    fs.readFile(__dirname + '/../../html/mision.html', 'utf8', function(err, text) {
+	        res.render('index', {
+        	    title : 'SDI - Sport Development Intelligence',
+        		graph: text
+        	});
+        });
+	});
+	
+	app.get('/values', function (req, res) {
+	    fs.readFile(__dirname + '/../../html/values.html', 'utf8', function(err, text) {
+	        res.render('index', {
+        	    title : 'SDI - Sport Development Intelligence',
+        		graph: text
+        	});
+        });
+	});
+	
+	app.get('/intro', function (req, res) {
+	    fs.readFile(__dirname + '/../../html/intro.html', 'utf8', function(err, text) {
+	        res.render('index', {
+        	    title : 'SDI - Sport Development Intelligence',
+        		graph: text
+        	});
+        });
+	});
+	
+	app.get('/services', function (req, res) {
+	    fs.readFile(__dirname + '/../../html/services.html', 'utf8', function(err, text) {
+	        res.render('index', {
+        	    title : 'SDI - Sport Development Intelligence',
+        		graph: text
+        	});
+        });
+	});
+	
+	app.get('/solutions', function (req, res) {
+	    fs.readFile(__dirname + '/../../html/solutions.html', 'utf8', function(err, text) {
+	        res.render('index', {
+        	    title : 'SDI - Sport Development Intelligence',
+        		graph: text
+        	});
+        });
+	});
+	
 };
