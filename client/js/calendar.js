e6c2d6f9b90f0440d61c58838f089b7baae3e4a0
despinalr <davidesr7@gmail.com>
2018-04-02 18:18:32 +0000

    Reserva

--- client/js/calendar.js
+++ client/js/calendar.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
+  
+    $("#botonReserva" ).click(function() {
+      $.ajax({
+        url: 'https://trial.bizagi.com/david.espinal/sdrc/webservices/workflowenginesoa.asmx/createCasesAsString?casesInfo=<BizAgiWSParam><domain>domain</domain><userName>admon</userName><Cases><Case><Process>Reservas</Process><Entities><Reservas><Nombre>Juan</Nombre><Correo>Juan@sdi.com</Correo><Telefono>3115672266</Telefono><FechaInicio>2018/10/12</FechaInicio><FechaFin>2018/10/13</FechaFin></Reservas></Entities></Case></Cases></BizAgiWSParam>',
+        type: 'GET',
+        success: function(data) { 
+            alert("Reserva Creada con Exito. Será contactado por nuestros asesores." );
+        },
+        error: function(jqXHR,error, errorThrown) {
+            alert('Error!: ' + jqXHR.status);
+        }
+      });
+    });
 
     $('#calendar').fullCalendar({
       defaultView: 'month',
@@ -21,43 +34,43 @@ $(document).ready(function() {
       events: [
         {
           title: 'America vs Junior',
-          start: '2018-03-01'
+          start: '2018-04-01'
         },
         {
           title: 'Concierto',
-          start: '2018-03-07',
-          end: '2018-03-10'
+          start: '2018-04-07',
+          end: '2018-04-10'
         },
         {
           id: 999,
           title: 'America de Cali Libertadores',
-          start: '2018-03-09T16:00:00'
+          start: '2018-04-09T16:00:00'
         },
         {
           title: 'Misión Carismática Internacional',
-          start: '2018-03-11',
-          end: '2018-03-13'
+          start: '2018-04-11',
+          end: '2018-04-13'
         },
         {
           title: 'Mantenimiento',
-          start: '2018-03-14T10:30:00',
-          end: '2018-03-15T12:30:00'
+          start: '2018-04-14T10:30:00',
+          end: '2018-04-15T12:30:00'
         },
         {
           title: 'Fiesta Banco de Occidente',
-          start: '2018-03-12T12:00:00'
+          start: '2018-04-12T12:00:00'
         },
         {
           title: 'Concierto Maluma',
-          start: '2018-03-22T14:30:00'
+          start: '2018-04-22T14:30:00'
         },
         {
           title: 'Vicente Fernandez',
-          start: '2018-03-13T07:00:00'
+          start: '2018-04-13T07:00:00'
         },
         {
           title: 'América vs Jaguares',
-          start: '2018-03-28T07:00:00'
+          start: '2018-04-28T07:00:00'
         }
       ]
       
